d2fa165a6e212212b08a30c5763a6cb15585c837
kdrv dev <ffeodalik@gmail.com>
2025-07-26 08:16:47 +0500

    upd:faviocn

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="uz">
        <head>
-        <link rel="icon" href="/favicon.png" />
+      <link rel="icon" href="/favicon.png" type="image/png" />
       </head>
       <body>{children}</body>
     </html>
